f13423fafaa3c5ec295a66969f5823996d9d4aaf
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2015-07-30 23:12:48 +0200

    Bugfixes

--- src/app/sms.controller.js
+++ src/app/sms.controller.js
@@ -1,31 +1,32 @@
 'use strict';
 
 angular.module('app').controller('SMSController', function($cordovaSms, $ionicPopup, $scope) {
-  $scope.vm = this;
+  var vm = $scope.vm = this;
 
-  this.recipient = null;
-  this.content = null;
+  vm.recipient = '';
+  vm.content = '';
 
-  this.submit = function() {
+  vm.submit = function() {
     var options = {
-        replaceLineBreaks: true, // true to replace \n by a new line, false by default
-        android: {
-            //intent: 'INTENT'  // send SMS with the native android SMS messaging
-            intent: '' // send SMS without open any other app
-        }
+      replaceLineBreaks: true, // true to replace \n by a new line, false by default
+      android: {
+        //intent: 'INTENT'  // send SMS with the native android SMS messaging
+        intent: '' // send SMS without open any other app
+      }
     };
 
     $cordovaSms
-      .send(this.recipient, this.content, options)
+      .send(vm.recipient, vm.content, options)
       .then(function() {
         $ionicPopup.alert({
           title: "Sukces",
           template: "SMS został wysłany"
         }).then(function() {
-          $scope.vm.recipient = null;
-          $scope.vm.content = null;
+          vm.recipient = null;
+          vm.content = null;
         });
-      }, function(error) {
+      })
+      .catch(function(error) {
         $ionicPopup.alert({
           title: "Błąd",
           template: "Nie udało się wysłać SMS.<br/>" + error
